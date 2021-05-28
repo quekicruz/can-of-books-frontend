a22803c792773956021ddd14be1eae52eb9b9a99
sunny-lee3 <sunnylee3712@gmail.com>
2021-05-27 20:17:44 -0700

    updated front end and completed

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
@@ -136,47 +137,47 @@ class MyFavoriteBooks extends React.Component {
   render() {
     return(
       <div>
-      <Jumbotron id="jumbo">
-        <h1>My Favorite Books</h1>
+      <Jumbotron style={{background:"url('https://data.whicdn.com/images/295847954/original.gif')", backgroundSize:'fill'}} id="jumbo">
+        <h1 id='title' style={{color: 'white'}}>My Favorite Books</h1>
         </Jumbotron>
-        <Form id="lookbooks">
+        <Form id="lookbooks"> 
+        <Button variant="info" onClick={this.retrieveBook}>Take A Look At My Books</Button>
           <FormGroup>
-        <Button variant="warning" onClick={this.retrieveBook}>Take A Look At My Books</Button>
         {this.state.books.length > 0
           ? this.state.books.map(item => {
             return (
               <BookDisplay
                 name={item.name}
                 description={item.description}
-               status={item.status}
+                status={item.status}
               />
             )
           })
           : null}
           </FormGroup>
           </Form>
-        <Form >
-          <FormGroup>
-          <Form.Label> <h4>Enter A New Book</h4> </Form.Label>
+        <Form id="formGroups" >
+          <FormGroup style={{margin: '10px'}}>
+          <Form.Label style={{marginBottom: '10px'}}> <h4>Enter A New Book</h4> </Form.Label>
           <Form.Control placeholder="Name Of Book" onChange={this.newBookName}></Form.Control>
           <Form.Control placeholder="Description Of Book" onChange={this.newBookDescription}></Form.Control>
           <Form.Control placeholder="Status Of Book" onChange={this.newBookStatus}></Form.Control>
-          <Button onClick={this.addBookData}>Add Book</Button>
+          <Button variant='success' style={{margin: '10px'}} onClick={this.addBookData}>Add Book</Button>
           </FormGroup>
 
-          <FormGroup>
+          <FormGroup style={{margin: '10px'}}>
           <FormLabel> <h4>Update A Book</h4> </FormLabel>
           <Form.Control placeholder="Update Book Name" onChange={this.updatedNameOfBook}></Form.Control>
           <Form.Control placeholder="Update Book Description" onChange={this.updatedBookDescription}></Form.Control>
           <Form.Control placeholder="Update Book Status" onChange={this.updatedBookStatus}></Form.Control>
-          <Button onClick={this.updateBook}>Update Specific Book </Button>
+          <Button variant='warning' style={{margin: '10px'}} onClick={this.updateBook}>Update Specific Book </Button>
           </FormGroup>
 
           
-          <FormGroup>
+          <FormGroup style={{margin: '10px'}}>
             <FormLabel> <h4>Remove A Book</h4> </FormLabel>
           <Form.Control placeholder="Book Title to delete" onChange={this.newDelete}></Form.Control>
-          <Button onClick={this.deleteBookData}>Remove Undesired Book</Button>
+          <Button variant='danger' style={{margin: '10px'}} onClick={this.deleteBookData}>Remove Undesired Book</Button>
           </FormGroup>
          </Form>
           </div>
--- src/BookDisplay.js
+++ src/BookDisplay.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
+import './BestBooks.css' 
 
 class RenderBook extends React.Component {
   render() {
     return (
-      <Card>
-        <Card.Text>Book Name: {this.props.name}</Card.Text>
-        <Card.Text>Book Description: {this.props.description}</Card.Text>
-        <Card.Text>Status Of Book: {this.props.status}</Card.Text>
+      <Card id="cardDisplay" style = {{height: '400px', width: '100px', display: 'inline-flex', marginTop: '10px', backgroundColor: 'maroon'}}>
+        <Card.Title style={{color: 'white'}}>Book Name: {this.props.name}</Card.Title>
+        <Card.Body> 
+          <Card.Text>Book Description: {this.props.description}</Card.Text>
+          <Card.Text>Status Of Book: {this.props.status}</Card.Text>
+        </Card.Body>
       </Card>
     )
   }
